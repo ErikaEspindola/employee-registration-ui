1c3d9203324a95aa0171781f69997c8c84e6ff4d
Erika Espindola <erikaaespindola@hotmail.com>
2020-06-28 20:31:19 -0300

    Lista de colaboradores

--- src/app/employees/list/list.component.ts
+++ src/app/employees/list/list.component.ts
@@ -1,8 +1,10 @@
+import { EmployeesService } from './../employees.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { map, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-list',
@@ -11,92 +13,30 @@ import { Observable } from 'rxjs';
 })
 export class ListComponent implements OnInit {
 
-  list = [
-    {
-      id: 1,
-      name: 'Érika',
-      charge: 'Desenvolvedor',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Front-end',
-      picture: 'https://material.angular.io/assets/img/examples/shiba1.jpg'
-    },
-    {
-      id: 2,
-      name: 'Giovanni',
-      charge: 'Cientista de Dados',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Data Science',
-      picture: 'https://http2.mlstatic.com/bernese-mountain-dog-filhotes-D_NQ_NP_972818-MLB27708880509_072018-O.webp'
-    },
-    {
-      id: 3,
-      name: 'Érika',
-      charge: 'Desenvolvedor',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Front-end',
-      picture: 'https://material.angular.io/assets/img/examples/shiba1.jpg'
-    },
-    {
-      id: 4,
-      name: 'Giovanni',
-      charge: 'Cientista de Dados',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Data Science',
-      picture: 'https://http2.mlstatic.com/bernese-mountain-dog-filhotes-D_NQ_NP_972818-MLB27708880509_072018-O.webp'
-    },
-    {
-      id: 5,
-      name: 'Érika',
-      charge: 'Desenvolvedor',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Front-end',
-      picture: 'https://material.angular.io/assets/img/examples/shiba1.jpg'
-    },
-    {
-      id: 6,
-      name: 'Giovanni',
-      charge: 'Cientista de Dados',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Data Science',
-      picture: 'https://http2.mlstatic.com/bernese-mountain-dog-filhotes-D_NQ_NP_972818-MLB27708880509_072018-O.webp'
-    },
-    {
-      id: 7,
-      name: 'Érika',
-      charge: 'Desenvolvedor',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Front-end',
-      picture: 'https://material.angular.io/assets/img/examples/shiba1.jpg'
-    },
-    {
-      id: 8,
-      name: 'Giovanni',
-      charge: 'Cientista de Dados',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Data Science',
-      picture: 'https://http2.mlstatic.com/bernese-mountain-dog-filhotes-D_NQ_NP_972818-MLB27708880509_072018-O.webp'
-    },
-    {
-      id: 9,
-      name: 'Érika',
-      charge: 'Desenvolvedor',
-      icon: 'fa fa-lightbulb-o',
-      team: 'Chapter Front-end',
-      picture: 'https://material.angular.io/assets/img/examples/shiba1.jpg'
-    }
-  ];
+  list: any[] = [];
 
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: EmployeesService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value))
     );
+
+    this.listEmployees();
+  }
+
+  listEmployees() {
+    this.service.listEmployees().subscribe((res: any) => {
+      this.list = res;
+      this.list.forEach(employee => {
+        employee.profilePicture = this.sanitizer.bypassSecurityTrustResourceUrl(employee.profilePicture.toString());
+      });
+    });
   }
 
   private _filter(value: string): string[] {
