6aeb114ec2638e52266f0d3182ced461144139c6
Erika Espindola <erikaaespindola@hotmail.com>
2020-06-29 11:11:12 -0300

    Aplicação de filtro na lista

--- src/app/employees/list/list.component.ts
+++ src/app/employees/list/list.component.ts
@@ -1,10 +1,11 @@
-import { EmployeesService } from './../employees.service';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, startWith } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+
+import { EmployeeDetail } from '../entities';
+import { EmployeesService } from './../employees.service';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -13,36 +14,22 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class ListComponent implements OnInit {
 
-  list: any[] = [];
+  list: EmployeeDetail[] = [];
+  filteredList: EmployeeDetail[] = [];
 
-  myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]>;
+  filter = new FormControl();
+
+  chips = [
+    { name: 'Nome', state: false },
+    { name: 'Cargo', state: false },
+    { name: 'Competências', state: false },
+    { name: 'Time', state: false }
+  ];
 
   constructor(private router: Router, private service: EmployeesService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    );
-
     this.listEmployees();
-  }
-
-  listEmployees() {
-    this.service.listEmployees().subscribe((res: any) => {
-      this.list = res;
-      this.list.forEach(employee => {
-        employee.profilePicture = this.sanitizer.bypassSecurityTrustResourceUrl(employee.profilePicture.toString());
-      });
-    });
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
   detail(id: number) {
@@ -51,5 +38,67 @@ export class ListComponent implements OnInit {
 
   addEmployee() {
     this.router.navigate(['/add-employee']);
+  }
+
+  listEmployees() {
+    this.service.listEmployees().subscribe((res: any) => {
+      this.list = res;
+
+      this.list.forEach(employee => {
+        employee.profilePicture = this.sanitizer.bypassSecurityTrustResourceUrl(employee.profilePicture.toString());
+        employee.skills = employee.skills.map(skill => skill.toLowerCase())
+      });
+
+      this.filteredList = this.list;
+    });
+  }
+
+  changeSelected(chip) {
+    this.chips.forEach(c => c.state = false);
+    chip.state = true;
+  }
+
+  filterList() {
+    this.filteredList = this.list;
+
+    if (this.chips.some(chip => chip.state)) {
+      this.chips.forEach((chip, i) => {
+        if (chip.state) {
+          this.defineFilter(i);
+        }
+      })
+    } else {
+      this.filteredList = this.filteredList.filter(el => {
+        return el.charge.toLowerCase().includes(this.filter.value.toLowerCase()) ||
+          el.skills.find(skill => skill.includes(this.filter.value.toLowerCase())) ||
+          el.name.toLowerCase().includes(this.filter.value.toLowerCase()) ||
+          el.team.toLowerCase().includes(this.filter.value.toLowerCase())
+      });
+    }
+  }
+
+  defineFilter(i: number) {
+    switch (i) {
+      case 0:
+        this.filteredList = this.filteredList.filter(el =>
+          el.name.toLowerCase().includes(this.filter.value.toLowerCase())
+        );
+        break;
+      case 1:
+        this.filteredList = this.filteredList.filter(el =>
+          el.charge.toLowerCase().includes(this.filter.value.toLowerCase())
+        );
+        break;
+      case 2:
+        this.filteredList = this.filteredList.filter(el =>
+          el.skills.find(skill => skill.includes(this.filter.value.toLowerCase()))
+        );
+        break;
+      case 3:
+        this.filteredList = this.filteredList.filter(el =>
+          el.team.toLowerCase().includes(this.filter.value.toLowerCase())
+        );
+        break;
+    }
   }
 }
